c1cf04fab1a20a9cd46c97e2ada893abeeecf636
Haysa Rodrigues <hjsilva@thoughtworks.com>
2019-05-05 15:42:08 -0300

    Updates

---
+++ precache-manifest.40cce6fea4e35704950499eff8b4ac5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e69c9acf3c037f4e6bade7ec0e8d39c3",
+    "url": "/haysarodrigues/index.html"
+  },
+  {
+    "revision": "60752dfa43409b244111",
+    "url": "/haysarodrigues/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/haysarodrigues/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "60752dfa43409b244111",
+    "url": "/haysarodrigues/static/js/main.88d352ad.chunk.js"
+  },
+  {
+    "revision": "02b338d649fafd16ee72",
+    "url": "/haysarodrigues/static/js/runtime~main.eca11fe3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/haysarodrigues/static/media/logo.5d5d9eef.svg"
+  }
+]);
