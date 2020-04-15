d3a6fbb54d29d357c15fe253715cd8fc88dfbb1c
haysarodrigues <rodrigues.haysa@gmail.com>
2020-04-15 16:17:40 -0300

    Fix about component so unit tests can pass

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -6,7 +6,7 @@ export default class About extends Component {
     render() {
         return (
             <div className='aboutContainer'>
-                <h1 className='storyTitle'>Quem fez esse site e essas fotos?</h1>
+                <h1 className='storyTitle aboutTitle'>Quem fez esse site e essas fotos?</h1>
                 <div className='imageCropper'>
                     <img className='aboutMeImage'
                          src={aboutMeImage}
@@ -14,16 +14,16 @@ export default class About extends Component {
                     />
                 </div>
 
-                <p className='paragraphsFont'>
+                <p className='paragraphsFont aboutDescription'>
                     <p>Uma cabeça inquieta e ao mesmo tempo vazia. Curiosa e
-                    entediada, encontrei na fotografia um escape enorme para
-                    fugir dos estresses do dia a dia, e descobri que com ela,
-                    eu consigo me conectar e enxeragar as coisas com outros olhares.
+                        entediada, encontrei na fotografia um escape enorme para
+                        fugir dos estresses do dia a dia, e descobri que com ela,
+                        eu consigo me conectar e enxeragar as coisas com outros olhares.
                     </p>
                     <p>
-                    Olhares esses que nem sempre fazem bem mas que abrem minha cabeça
-                    pra entender melhor que na vida nem tudo deve ser problematização.
-                    Se não a gente endoida.
+                        Olhares esses que nem sempre fazem bem mas que abrem minha cabeça
+                        pra entender melhor que na vida nem tudo deve ser problematização.
+                        Se não a gente endoida.
                     </p>
                 </p>
 
