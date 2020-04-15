2a797643873a55e7956dc01e88ccf3313161ead8
haysarodrigues <rodrigues.haysa@gmail.com>
2020-04-15 15:23:58 -0300

    Updates

---
+++ precache-manifest.f49767858b37d63fd8f2243a49e6aa21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cbb146ecb8740e62086f01683b3e488",
+    "url": "/haysarodrigues/index.html"
+  },
+  {
+    "revision": "8508bc4719ea3b9b57af",
+    "url": "/haysarodrigues/static/css/main.be1cf525.chunk.css"
+  },
+  {
+    "revision": "2e7994767b045863f54c",
+    "url": "/haysarodrigues/static/js/2.0b220190.chunk.js"
+  },
+  {
+    "revision": "dbe5ca7a82c728776bb536c3d2d64135",
+    "url": "/haysarodrigues/static/js/2.0b220190.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8508bc4719ea3b9b57af",
+    "url": "/haysarodrigues/static/js/main.cdca4d79.chunk.js"
+  },
+  {
+    "revision": "05602bc0bca30b76f3e4",
+    "url": "/haysarodrigues/static/js/runtime-main.7b9f0a98.js"
+  },
+  {
+    "revision": "982f39035f85feef9ff9cd976e9eb34d",
+    "url": "/haysarodrigues/static/media/pp.982f3903.jpeg"
+  }
+]);
