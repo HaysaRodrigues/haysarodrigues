f6544d973c328f6a4a0c0e8e5fc344f186443404
haysarodrigues <rodrigues.haysa@gmail.com>
2020-04-23 22:02:01 -0300

    Make main image responsive

--- src/components/mainImage/MainImage.js
+++ src/components/mainImage/MainImage.js
@@ -1,13 +1,13 @@
 import React from 'react'
+import './MainImage.css'
 
 function MainImage() {
     return (
-        <div className="mainImage">
+        <div className="containerImage">
             <img
+                className='mainImage'
                 src="https://upload.wikimedia.org/wikipedia/commons/7/74/Acrobatic_tissue%2C_artistic_acrobatic_tissue.jpg"
-                alt="man doing artistic acrobatic tissue"
-                height="660"
-                width="100%">
+                alt="man doing artistic acrobatic tissue">
             </img>
         </div>
     )
