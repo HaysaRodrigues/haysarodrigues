6fee07423e95852eda61a999153f327fa09ca3c3
haysarodrigues <rodrigues.haysa@gmail.com>
2020-04-23 21:04:54 -0300

    Make paragraphs responsive

--- src/components/story/Story.js
+++ src/components/story/Story.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react'
 import StoryPhotoGallery from './StoryPhotoGallery'
+import './Story.css'
 
 class Story extends Component {
 
     render() {
         return (
-            <div className='story'>
+            <>
                 <h1 className='storyTitle'> {this.props.title} </h1>
                 <div className='paragraphs'>
                     <h3 className='paragraphsFont'> {this.props.firstParagraph} </h3>
@@ -14,7 +15,7 @@ class Story extends Component {
                 <div className='photoGrid'>
                     <StoryPhotoGallery photos={this.props.photos}/>
                 </div>
-            </div>
+            </>
         )
     }
 }
